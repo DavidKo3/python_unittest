3e6ff43a97f66f0222787f6e67d0f7565252cae1
tensor <tensor@tensor-VirtualBox>
2016-12-14 10:47:07 +0900

    uniitest_code_python

---
+++ src/simple_unittest.py
@@ -0,0 +1,14 @@
+import unittest
+import myCalc
+
+class MycalcTest(unittest.TestCase):
+    
+    def test_add(self):
+        c = myCalc.add(20,10)
+        self.assertEqual(c, 30)
+    def test_substract(self):
+        c = myCalc.substract(20,10)
+        self.assertEqual(c, 110)   
+        
+if __name__ == '__main__':
+    unittest.main()
